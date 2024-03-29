0d63f2a78148caca868f6ec32b2e4052f96f5095
Prasantaclifav <prasanta.patra@clifav.com>
2024-03-30 01:29:43 +0530

    Jwt

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,10 +1,17 @@
 package com.example.demo.entity;
 
+import java.util.Collection;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import lombok.Data;
 import lombok.Getter;
@@ -14,7 +21,7 @@ import lombok.Setter;
 @Entity
 @Data
 @Table(name="user")
-public class User {
+public class User implements UserDetails {
 
 @Id
 @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,12 +31,33 @@ private String name;
 private String email;
 private String password;
 private UserRole userRole;
-	
-	
-	
-	
-	
-	
-	
+
+
+
+@Override
+public Collection<? extends GrantedAuthority> getAuthorities() {
+	return List.of(new SimpleGrantedAuthority(userRole.name()));
+}
+@Override
+public String getUsername() {
+	return email;
+}
+@Override
+public boolean isAccountNonExpired() {
+	return true;
+}
+@Override
+public boolean isAccountNonLocked() {
+	return true;
+}
+@Override
+public boolean isCredentialsNonExpired() {
+	return true;
+}
+@Override
+public boolean isEnabled() {
+	return true;
+}
+
 	
 }
--- src/main/java/com/example/demo/utils/JWTutils.java
+++ src/main/java/com/example/demo/utils/JWTutils.java
@@ -1,35 +1,69 @@
-/*
- * package com.example.demo.utils;
- * 
- * import java.util.Date; import java.util.HashMap; import java.util.Map; import
- * java.util.function.Function;
- * 
- * import org.springframework.security.core.userdetails.UserDetails; import
- * org.springframework.stereotype.Component;
- * 
- * import io.jsonwebtoken.Claims; import io.jsonwebtoken.Jwts; import
- * io.jsonwebtoken.SignatureAlgorithm;
- * 
- * @Component public class JWTutils {
- * 
- * public String extractUserName(String token) { return
- * extractClaim(token,Claims::getSubject); }
- * 
- * public String generateToken(UserDetails userDetails) { return
- * generateToken(new HashMap<>(),userDetails); }
- * 
- * public boolean isTokenValid(String token,UserDetails userDetails) { final
- * String UserName = extractUserName(token); return
- * (UserName.equals(UserDetails.getUsername()))&& !isTokenExpired(token); }
- * private<T> T extractClaim(String token, Function<Claims,T>claimsResolvers) {
- * final Claims claims = extractAllClaims(token); return
- * claimsResolvers.apply(claims); } public String
- * generateToken(Map<String,Object>extraClaims, UserDetails userDetails) {
- * return
- * Jwts.builder().setClaims(extraClaims).setSubject(userDetails.getUsername())
- * .setIssuedAt(new Date(System.currentTimeMillis())) .setExpiration(new
- * Date(System.currentTimeMillis()+1000*60*24))
- * .signWith(getSigningKey(),SignatureAlgorithm.HS256).compact(); }
- * 
- * }
- */
+
+  package com.example.demo.utils;
+  
+  import java.security.Key;
+  import java.util.Date;
+  import java.util.HashMap; 
+  import java.util.Map; 
+  import java.util.function.Function;
+  
+  import org.springframework.security.core.userdetails.UserDetails; 
+  import org.springframework.stereotype.Component;
+  
+  import io.jsonwebtoken.Claims; 
+  import io.jsonwebtoken.Jwts; 
+  import io.jsonwebtoken.SignatureAlgorithm;
+  import io.jsonwebtoken.io.Decoders;
+  import io.jsonwebtoken.security.Keys;
+  
+  @Component 
+  public class JWTutils {
+  
+  public String extractUserName(String token) {
+	  
+  return extractClaim(token,Claims::getSubject); 
+  }
+  
+  public String generateToken(UserDetails userDetails) { 
+	  
+  return generateToken(new HashMap<>(),userDetails); 
+  }
+  
+  public boolean isTokenValid(String token,UserDetails userDetails) {
+  final String UserName = extractUserName(token); 
+  return(UserName.equals(userDetails.getUsername()))&& !isTokenExpired(token); 
+  }
+  private<T> T extractClaim(String token, Function<Claims,T>claimsResolvers) {
+  final Claims claims = extractAllClaims(token);
+  return claimsResolvers.apply(claims); 
+  }
+  
+  public String generateToken(Map<String,Object>extraClaims, UserDetails userDetails) {
+  return Jwts.builder().setClaims(extraClaims)
+		               .setSubject(userDetails.getUsername())
+                       .setIssuedAt(new Date(System.currentTimeMillis())) 
+                       .setExpiration(new Date(System.currentTimeMillis()+ 1000 * 60 * 24))
+                       .signWith(getSigningKey(),SignatureAlgorithm.HS256).compact(); 
+  }
+  public String generateRefreshToken(Map<String,Object>extraClaims, UserDetails userDetails) {
+	  return Jwts.builder().setClaims(extraClaims).setSubject(userDetails.getUsername())
+			               .setIssuedAt(new Date(System.currentTimeMillis()))
+			               .setExpiration(new Date(System.currentTimeMillis() + 604800000))
+			               .signWith(getSigningKey(),SignatureAlgorithm.HS256).compact(); 
+  }
+  
+  public boolean isTokenExpired(String token) {
+	  return extractExpiration(token).before(new Date());
+  }
+  private Date extractExpiration(String token) {
+	  return extractClaim(token, Claims::getExpiration);
+  }
+  private Claims extractAllClaims(String token) {
+	  return Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parseClaimsJws(token).getBody();
+  }
+  private Key getSigningKey() {
+	  byte[]keyBytes = Decoders.BASE64.decode("413F4428472B4863654G121464R27613479A9976");
+	  return Keys.hmacShaKeyFor(keyBytes);
+  }
+  }
+ 
